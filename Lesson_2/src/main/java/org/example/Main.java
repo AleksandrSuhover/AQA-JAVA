994db802bbbdc77b2327311a690d6dc791b74fdb
Alexandr Sukhover <aleksandr.sukhover@gmail.com>
2023-12-14 15:37:23 +0300

    Homework Lesson_2 Version 2.0(Short)

--- Lesson_2/src/main/java/org/example/Main.java
+++ Lesson_2/src/main/java/org/example/Main.java
@@ -1,48 +1,28 @@
-package org.example;
+//Данная версия ДЗ написана в стиле минимализма. В предыдущем коммите есть более читабельная версия)
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
         printColor();
         compareNumbers();
     }
 
-    public static void printThreeWords()
-    {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+    public static void printThreeWords() {
+        System.out.println("Orange" + "\n" + "Banana" + "\n" + "Apple");
     }
 
-    public static void checkSumSign()
-    {
-        int a = 5;
-        int b = -5;
-
-        String result = (a + b) >= 0 ? "Сумма положительная" : "Сумма отрицательная";
-        System.out.println(result);
+    public static void checkSumSign() {
+        int a = 5, b = -5;
+        System.out.println((a + b) >= 0 ? "Сумма положительная" : "Сумма отрицательная");
     }
 
-    public static void printColor()
-    {
+    public static void printColor() {
         int value = 100;
-
-        if(value <= 0){
-            System.out.println("Красный");
-        } else if(value <= 100){
-            System.out.println("Жёлтый");
-        } else {
-            System.out.println("Зелёный");
-        }
+        System.out.println(value <= 0 ? "Красный" : value <= 100 ? "Жёлтый" : "Зелёный");
     }
 
-    public static void compareNumbers()
-    {
-        int a = 5;
-        int b = 6;
-
-        String result = a >= b ? "a >= b" : "a < b";
-        System.out.println(result);
+    public static void compareNumbers() {
+        int a = 5, b = 5;
+        System.out.println(a >= b ? "a >= b" : "a < b");
     }
 }
